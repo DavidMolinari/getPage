9cdc4b64705ce88739a315b7ac16965f2085833e
David MOLINARI <contact@davidmolinari.fr>
2018-02-16 16:11:55 +0100

    update meh

--- lib/getPage.php
+++ lib/getPage.php
@@ -7,21 +7,29 @@
  * @param $headers
  * @return mixed
  */
+/**
+ * @param $url
+ * @param $postFields
+ * @param $headers
+ * @return mixed
+ */
 function getPage($url, $postFields = null, $headers = null)
 {
     $useragent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.89 Safari/537.36';
     $timeout = 120;
-    $dir = dirname(__FILE__);
-    $cookie_file = $dir . '/cookies/' . md5($_SERVER['REMOTE_ADDR']) . '.txt';
     $ch = curl_init($url);
-    if($headers !=null) fcurl_setopt($ch, CURLOPT_HEADER, $headers);
-    curl_setopt($ch, CURLOPT_COOKIEFILE, $cookie_file);
-    curl_setopt($ch, CURLOPT_COOKIEJAR, $cookie_file);
+    if($headers !=null) 
+        curl_setopt($ch, CURLOPT_HEADER, $headers);
+    
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-    if($postFields != null) curl_setopt($ch, CURLOPT_POSTFIELDS, $postFields);
+    if($postFields != null){
+        curl_setopt($ch, CURLOPT_POST, 1);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $postFields);
+    }
     curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
     curl_setopt($ch, CURLOPT_ENCODING, "");
-    curl_setopt($ch, CURLOPT_HTTPHEADER, ['Accept-Language: fr']);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+    curl_setopt($ch, CURLOPT_SSLVERSION, 4);
     curl_setopt($ch, CURLOPT_AUTOREFERER, true);
     curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, $timeout);
     curl_setopt($ch, CURLOPT_TIMEOUT, $timeout);
